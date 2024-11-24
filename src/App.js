056c5987a2f2a7d71c8b42f17019bdc2ea67f190
maqa55 <maqamet8@gmail.com>
2024-11-24 23:36:19 +0400

    calculyator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+ import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ function App() {
+     const [firstNumber,setFirstNumber] = useState('0')
+     const [secondNum,setSecondNum] = useState('')
+     const [operator,setOperator] = useState('')
 
-export default App;
+     const enterNum = (number) =>{
+         if(operator!='') {
+             setSecondNum(Number(secondNum.toString()+number))
+         }else {
+             setFirstNumber(Number(firstNumber.toString()+number))
+         }
+     }
+
+     const chosseOperator = (oper) => {
+         setOperator(oper)
+     }
+
+
+     const resultBtn = () => {
+         if(operator === '+') {
+             setFirstNumber(firstNumber+secondNum)
+         }else if (operator === '-'){
+             setFirstNumber(firstNumber-secondNum)
+         }else if (operator ==='x') {
+             setFirstNumber(firstNumber*secondNum)
+         }else if (operator ==='/') {
+             setFirstNumber (firstNumber/secondNum)
+         }
+         setSecondNum('')
+         setOperator('')   
+     }
+
+     const faiz = () => {
+         setFirstNumber(firstNumber/100)
+     }
+
+     const clearBoard = () => {
+         setFirstNumber('0')
+         setSecondNum('')
+        setOperator('')
+     }
+
+
+   return (
+     <div>
+         <section>
+         <h1>
+            {firstNumber}{operator}{secondNum}
+         </h1>
+     </section>
+     <section>
+         <div className="calc_column">
+             <div className="calc_row">
+                 <button onClick={() => clearBoard()}>
+                     C
+                 </button>
+                 <button>
+                     +/-
+                 </button>
+                 <button onClick={() => faiz()}>
+                     %
+                 </button>
+                 <button onClick={()=> chosseOperator('/')}>
+                     &divide;
+                 </button>
+             </div>
+             <div className="calc_row">
+                 <button onClick={()=> enterNum(7)}>
+                     7
+                 </button>
+                 <button onClick={()=> enterNum(8)}>
+                     8
+                 </button>
+                 <button onClick={()=> enterNum(9)}>
+                     9
+                 </button>
+                 <button onClick={()=> chosseOperator('x')}>
+                     X
+                 </button>
+             </div>
+             <div className="calc_row">
+                 <button onClick={()=> enterNum(4)}>
+                     4
+                 </button>
+                 <button onClick={()=> enterNum(5)}>
+                     5
+                 </button>
+                 <button onClick={()=> enterNum(6)}>
+                     6
+                 </button>
+                 <button onClick={()=> chosseOperator('-')}>
+                     -
+                 </button>
+             </div>
+             <div className="calc_row">
+                 <button onClick={()=> enterNum(1)}>
+                     1
+                 </button>
+                 <button onClick={()=> enterNum(2)}>
+                     2
+                 </button>
+                 <button onClick={()=> enterNum(3)}>
+                     3
+                 </button>
+                 <button onClick={() => chosseOperator('+')}>
+                     +
+                 </button>
+             </div>
+             <div className="calc_row ">
+                 <button id="zero" onClick={()=> enterNum(0)}>
+                     0
+                 </button>
+                 <button>
+                     ,
+                 </button>
+                 <button onClick={() => resultBtn()}>
+                     =
+                 </button>
+             </div>
+         </div>
+     </section>
+     </div>
+   )
+ }
+
+ export default App
---
+++ src/Cal.js
@@ -0,0 +1,126 @@
+import { useState } from "react";
+
+function Cal() {
+    const [firstNumber, setFirstNumber] = useState('0')
+    const [secondNum, setSecondNum] = useState('')
+    const [operator, setOperator] = useState('')
+
+    const enterNum = (number) => {
+        if (operator != '') {
+            setSecondNum(Number(secondNum.toString() + number))
+        } else {
+            setFirstNumber(Number(firstNumber.toString() + number))
+        }
+    }
+
+    const chosseOperator = (oper) => {
+        setOperator(oper)
+    }
+
+    const resultBtn = () => {
+        if (operator === '+') {
+            setFirstNumber(firstNumber + secondNum)
+        } else if (operator === '-') {
+            setFirstNumber(firstNumber - secondNum)
+        } else if (operator === 'x') {
+            setFirstNumber(firstNumber * secondNum)
+        } else if (operator === '/') {
+            setFirstNumber(firstNumber / secondNum)
+        }
+        setSecondNum('')
+        setOperator('')
+    }
+    const faiz = () => {
+        setFirstNumber(firstNumber / 100)
+    }
+
+    const clearBoard = () => {
+        setFirstNumber('0')
+        setSecondNum('')
+        setOperator('')
+    }
+
+    return (
+        <div>
+
+            <section>
+                <h1>
+                    {firstNumber}{operator}{secondNum}
+                </h1>
+            </section>
+            <section>
+                <div className="calc-column">
+                    <div className="calc-row">
+                        <button onClick={() => clearBoard()}>
+                            C
+                        </button>
+                        <button>
+                            +/-
+                        </button>
+                        <button onClick={() => faiz()}>
+                            %
+                        </button>
+                        <button onClick={() => chosseOperator("/")}>
+                            &divide;
+                        </button>
+                    </div>
+                    <div className="clac_row">
+                        <button onClick={() => enterNum(7)}>
+                            7
+                        </button>
+                        <button onClick={() => enterNum(8)}>
+                            8
+                        </button>
+                        <button onClick={() => enterNum(9)}>
+                            9
+                        </button>
+                        <button onClick={() => chosseOperator('x')}>
+                            X
+                        </button>
+                    </div>
+                    <div className="calc_row">
+                        <button onClick={() => enterNum(4)}>
+                            4
+                        </button>
+                        <button onClick={() => enterNum(5)}>
+                            5
+                        </button>
+                        <button onClick={() => enterNum(6)}>
+                            6
+                        </button>
+                        <button onClick={() => chosseOperator('-')}>
+                            -
+                        </button>
+                    </div>
+                    <div className="clac_row">
+                        <button onClick={() => enterNum(1)}>
+                            1
+                        </button>
+                        <button onClick={() => enterNum(2)}>
+                            2
+                        </button>
+                        <button onClick={() => enterNum(3)}>
+                            3
+                        </button>
+                        <button onClick={() => chosseOperator('+')}>
+                            +
+                        </button>
+                    </div>
+                    <div className="calc_row">
+                        <button id="zero" onClick={() => enterNum(0)}>
+                            0
+                        </button>
+                        <button>
+                            ,
+                        </button>
+                        <button onClick={() => resultBtn()}>
+                            =
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+
+}
+export default Cal
